dff598ac7f371ba424f2539cdf82b55f3084b755
Sachin Nishad <sachin.nishad@businessnext.com>
2023-08-09 15:07:05 +0530

    add

--- deletedlogs.sh
+++ deletedlogs.sh
@@ -3,8 +3,8 @@
 # Get the current date in seconds since the epoch
 current_date=$(date +%s)
 
-# Calculate the timestamp for 1 minute ago
-one_minute_ago=$((current_date - 60))  # 1 minute in seconds
+# Calculate the timestamp for 30 seconds ago
+thirty_seconds_ago=$((current_date - 30))  # 30 seconds in seconds
 
 # File to store the logs
 log_file="deleted_branches.log"
@@ -12,12 +12,14 @@ log_file="deleted_branches.log"
 # Clear the log file if it exists
 > "$log_file"
 
-# Iterate through local branches and delete those created within the last 1 minute
+# Iterate through local branches
 for branch in $(git branch --format='%(refname:short)'); do
-    branch_date=$(git log -n 1 --format="%at" "$branch")
-    if [ $branch_date -gt $one_minute_ago ]; then
-        git branch -D "$branch"
-        echo "Deleted branch $branch" >> "$log_file"
+    if [ "$branch" != "main" ]; then
+        branch_date=$(git log -n 1 --format="%at" "$branch")
+        if [ $branch_date -gt $thirty_seconds_ago ]; then
+            git branch -D "$branch"
+            echo "Deleted branch $branch" >> "$log_file"
+        fi
     fi
 done
 
